9999c1be536fe0c5f7f87ca8087ede247d91b9e6
Henry Peeples <hgpeeples48@gmail.com>
2023-01-28 00:33:38 -0600

    Add directory traversal and cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::env;
+use clap::Parser;
+use std::{env, fs, path::PathBuf};
 
-mod traversal;
-use traversal::traverse_dir;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    dir: String,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
+
     let current_dir = env::current_dir().expect("pwd err");
 
-    traverse_dir(current_dir);
+    traverse_dir(current_dir, &args);
+}
+
+fn traverse_dir(path: PathBuf, args: &Args) {
+    for entry in fs::read_dir(path).expect("read_dir err") {
+        let entry = entry.expect("entry err");
+        let meta = fs::metadata(entry.path()).expect("metadata err");
+
+        if meta.is_file() {
+            println!("{:?}", entry);
+        } else {
+            traverse_dir(entry.path(), &args);
+        }
+    }
 }
